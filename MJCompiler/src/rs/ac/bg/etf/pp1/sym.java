1d6e11bf781a678dfe3e305a3f63f1e3e23ec169
sivizdral <ivancvetic@outlook.com>
2022-12-13 18:05:37 +0100

    Initial commit

---
+++ MJCompiler/src/rs/ac/bg/etf/pp1/sym.java
@@ -0,0 +1,64 @@
+package rs.ac.bg.etf.pp1;
+
+public class sym {
+
+	// Keywords
+	public static final int PROGRAM = 1;
+	public static final int BREAK = 2;
+	public static final int CLASS = 3;
+	public static final int ENUM = 4;
+	public static final int ELSE = 5;
+	public static final int CONST = 6;
+	public static final int IF = 7;
+	public static final int DO = 8;
+	public static final int WHILE = 9;
+	public static final int NEW = 10;
+	public static final int PRINT = 11;
+	public static final int READ = 12;
+	public static final int RETURN = 13;
+	public static final int VOID = 14;
+	public static final int EXTENDS = 15;
+	public static final int CONTINUE = 16;
+	public static final int THIS = 17;
+	public static final int FOREACH = 18;
+	
+	// Identifiers
+	public static final int IDENT = 19;
+	
+	// Constants
+	public static final int NUMCONST = 20;
+	public static final int CHARCONST = 21;
+	public static final int BOOLCONST = 22;
+	
+	// Operators
+	public static final int PLUS = 23;
+	public static final int MINUS = 24;
+	public static final int MUL = 25;
+	public static final int DIV = 26;
+	public static final int PERCENT = 27;
+	public static final int EQUALS_EQUALS = 28;
+	public static final int NOT_EQUALS = 29;
+	public static final int GT = 30;
+	public static final int GTE = 31;
+	public static final int LT = 32;
+	public static final int LTE = 33;
+	public static final int AND = 34;
+	public static final int OR = 35;
+	public static final int EQUALS = 36;
+	public static final int INCREMENT = 37;
+	public static final int DECREMENT = 38;
+	public static final int SEMICOLON = 39;
+	public static final int COLON = 40;
+	public static final int COMMA = 41;
+	public static final int DOT = 42;
+	public static final int LEFT_ROUND_PARENTHESES = 43;
+	public static final int RIGHT_ROUND_PARENTHESES = 44;
+	public static final int LEFT_SQUARE_PARENTHESES = 45;
+	public static final int RIGHT_SQUARE_PARENTHESES = 46;
+	public static final int LEFT_WIGGLY_PARENTHESES = 47;
+	public static final int RIGHT_WIGGLY_PARENTHESES = 48;
+	public static final int ARROW = 49;
+	
+	public static final int EOF = 50;
+	
+}
